ad189bca034f24f2fd51395a1b54adbe79a66dca
goboden <goboden@yandex.ru>
2021-09-13 16:50:00 +0300

    Задание 2 Исправление

--- week3/practice_2_datetime.py
+++ week3/practice_2_datetime.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 import locale
 
 
-def main():
+def print_days():
     dt_now = datetime.now()
     dt_yesterday = dt_now - timedelta(days=1)
     dt_30_days_ago = dt_now - timedelta(days=30)
@@ -12,10 +12,11 @@ def main():
     print('Сегодня: {}'.format(dt_now.strftime('%d %B %Y')))
     print('30 дней назад: {}'.format(dt_30_days_ago.strftime('%d %B %Y')))
 
-    date_string = '01/01/25 12:10:03.234567'
-    dt_from_string = datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f')
-    print('Дата {}, тип {}'.format(dt_from_string, type(dt_from_string)))
+
+def str_2_datetime(date_string):
+    return datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f')
 
 
 if __name__ == '__main__':
-    main()
+    print_days()
+    print(str_2_datetime("01/01/20 12:10:03.234567"))
