eff784bb66e22580801241055e7763cdb2688f5b
goboden <goboden@yandex.ru>
2021-09-22 20:44:42 +0300

    Уровень 2 Задание 4

--- week3/level_2_3/calc.py
+++ week3/level_2_3/calc.py
@@ -1,2 +1,17 @@
+def is_valid_symbol(symbol: str):
+    valid_symbols = '1234567890+-*/()'
+    return symbol in valid_symbols
+
+
+def text_to_expression(text):
+    expression = [x for x in text if is_valid_symbol(x)]
+    return ''.join(expression)
+
+
 def reply(context, user_id, user_text: str):
-    pass
+    expression_text = text_to_expression(user_text)
+    try:
+        result = eval(expression_text)
+        return f'{expression_text} = {result}'
+    except ZeroDivisionError:
+        return f'Ошибка. Деление на 0. {expression_text}'
